76d682c257dfd997b5962c3b744f4e72b6ab5c94
Anthony Gualtieri <agualtieri112@gmail.com>
2018-05-15 14:38:40 -0400

    Made play button that appears on hover and on hover over button changes color

--- scripts/album.js
+++ scripts/album.js
@@ -62,7 +62,7 @@ var albumJT = {
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -94,11 +94,17 @@ var albumJT = {
 	}
 };
 
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+ var songRows = document.getElementsByClassName('album-view-song-item');
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
 	setCurrentAlbum(albumMichealScott);
 
-var albums = [albumPicasso, albumMarconi, albumJT, albumMichealScott];
-var index = 1;
+ var albums = [albumPicasso, albumMarconi, albumJT, albumMichealScott];
+ var index = 1;
 	albumImage.addEventListener("click", function(event) {
 		setCurrentAlbum(albums[index]);
 		index++;
@@ -106,4 +112,15 @@ var index = 1;
 			index = 0;
 		}
 	});
+	songListContainer.addEventListener('mouseover', function(event) {
+		if (event.target.parentElement.className === 'album-view-song-item') {
+			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+		}
+		});
+
+		for (var i = 0; i < songRows.length; i++) {
+		songRows[i].addEventListener('mouseleave', function(event) {
+this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+		});
+	}
 };
